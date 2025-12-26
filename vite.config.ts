9b7448dd4f29ed94716c124dde18211a2ab5953f
Lostless1907 <206261279+Lostless1907@users.noreply.github.com>
2025-12-26 14:46:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-  preview: {
-    host: true,
-    port: 3000,
-    allowedHosts: "all"
-  }
 })
